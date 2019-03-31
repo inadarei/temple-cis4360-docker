10dd6544c30c6cf0f55cad1366afef789d3a9fe9
Irakli Nadareishvilii <inadarei@users.noreply.github.com>
2019-03-31 01:49:10 -0400

    init

---
+++ example01/server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h2>Hello, class CIS 4360!</h2>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0') 
